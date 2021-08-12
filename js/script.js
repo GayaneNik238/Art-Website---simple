8cef71e60c3285b76b4f617fe92dc5e136f9c7d5
GayaneNik238 <gnikoghosyan98@gmail.com>
2021-08-12 17:25:29 +0400

    arty simple website

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector(".two").addEventListener("click", function() {
+    document.getElementById("big-img").style.backgroundImage = "url('./images/2.jpg')";
+});
+
+document.querySelector(".three").addEventListener("click", function() {
+    document.getElementById("big-img").style.backgroundImage = "url('./images/3.jpg')";
+});
+
+document.querySelector(".four").addEventListener("click", function() {
+    document.getElementById("big-img").style.backgroundImage = "url('./images/4.jpg')";
+});
+
+document.querySelector(".five").addEventListener("click", function() {
+    document.getElementById("big-img").style.backgroundImage = "url('./images/5.jpg')";
+});
